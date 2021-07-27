17cbf5676e9d70dbce5bf3d9995686d9b120c1e8
xander-gideon <xander@gideononline.com>
2021-07-27 15:52:54 -0700

    Include Sphinx assets directory

--- copy-docs.sh
+++ copy-docs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 PYTHON_DOCS_SRC_PATH="$HOME/gideon/gideon-api-python/docs/build/html/"
 rsync -r --delete $PYTHON_DOCS_SRC_PATH docs/python
-# Empty HTML file for GitHub Pages deployment
-cp docs/index.html docs/python/_static
